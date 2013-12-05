2651ca56a99e8f0d579e9109c3257086dba3d62b
Juan Carlos Condori Machicado <juanc.com@gmail.com>
2013-12-05 09:10:10 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OapDocuments::Application.config.secret_token = '2b405ebb8d09c5e2b3927e1e8edebcdf1b33bc1f58eb9331b7d86368085a4a12eb5d54f5b4b860bb82d61c7a79789c8d94ab26ff2e5fc2a1dde8d3337f5be580'
